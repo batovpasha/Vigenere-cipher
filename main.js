882e419c4a7263257dc79d9904ddea779bc77181
batovpasha <batovpasha@gmail.com>
2018-09-06 23:53:17 +0300

    Some code fixes

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const readData = (fn) => {
     input: process.stdin,
     output: process.stdout
   });
+
   rl.question('Enter your key: ', (key) => {
     rl.question('Enter your alphabet: ', (alphabet) => {
       rl.question('Enter your message: ', (message) => {
@@ -19,22 +20,29 @@ const readData = (fn) => {
 
 const decipher = (cipher) => {
   const decipherMessage = [];
+  
   for (const i in cipher) {
     const row = matrix[0].indexOf(base[i]);
     const column = matrix[row].indexOf(cipher[i]);
+    
     decipherMessage.push(matrix[0][column]);
   }
+
   console.log(`deciphered message: ${decipherMessage.join('')}`);
 };
 
 const encryption = (message, matrix) => {
   const cipher = [];
+
   for (const i in message) {
     const column = matrix[0].indexOf(message[i]);
     const row = matrix[0].indexOf(base[i]);
+    
     cipher.push(matrix[row][column]);
   }
+  
   console.log(`cipher: ${cipher.join('')}`);
+  
   decipher(cipher);
 };
 
@@ -42,12 +50,16 @@ const matrix = [];
 
 const createMatrix = (alphabet, message) => {
   const alphabetArr = alphabet.split('');
+  
   matrix.push(alphabetArr.slice());
+  
   for (let i = 1; i < alphabetArr.length; i++) {
     alphabetArr.push(alphabetArr[0]);
     alphabetArr.shift();
+    
     matrix.push(alphabetArr.slice());
   }
+  
   console.log(matrix);
   encryption(message, matrix);
 };
